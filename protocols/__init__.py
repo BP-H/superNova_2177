337f8cf3fe61538c415bcfa8148ea01e7476a7a6
BP-H <blueprinthustler@gmail.com>
2025-07-28 02:08:05 -0400

    Create __init__.py
    Signed-off-by: BP-H <blueprinthustler@gmail.com>

---
+++ protocols/__init__.py
@@ -0,0 +1,12 @@
+# protocols/__init__.py
+
+from .agent_protocols_suite import (
+    AgentProfile,
+    AgentTaskContract,
+    self_reflect,
+    ping_agent,
+    handshake,
+    fork_agent,
+    ValidatorElf,
+    DreamWeaver,
+)
