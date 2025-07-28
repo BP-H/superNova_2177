4ade54fe998142c71493257cc6cedba6d1a00b97
BP-H <blueprinthustler@gmail.com>
2025-07-28 13:19:45 -0400

    Add UI hook modules and tests

--- frontend_bridge.py
+++ frontend_bridge.py
@@ -29,6 +29,16 @@ from hypothesis.ui_hook import (
     rank_hypotheses_by_confidence_ui,
     detect_conflicting_hypotheses_ui,
 )
+from hypothesis_meta_evaluator_ui_hook import trigger_meta_evaluation_ui
+from hypothesis_reasoner_ui_hook import auto_flag_stale_ui
+from validation_certifier_ui_hook import run_integrity_analysis_ui
+from validator_reputation_tracker_ui_hook import update_reputations_ui
+from consensus_forecaster_agent_ui_hook import forecast_consensus_ui
 
 register_route("rank_hypotheses_by_confidence", rank_hypotheses_by_confidence_ui)
 register_route("detect_conflicting_hypotheses", detect_conflicting_hypotheses_ui)
+register_route("trigger_meta_evaluation", trigger_meta_evaluation_ui)
+register_route("auto_flag_stale", auto_flag_stale_ui)
+register_route("run_integrity_analysis", run_integrity_analysis_ui)
+register_route("update_reputations", update_reputations_ui)
+register_route("forecast_consensus_agent", forecast_consensus_ui)
