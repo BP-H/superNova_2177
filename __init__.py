464ec069a0c0f86669d3253961d41d6e56ad2460
BP-H <blueprinthustler@gmail.com>
2025-07-29 12:29:24 -0400

    Update __init__.py
    Signed-off-by: BP-H <blueprinthustler@gmail.com>

--- __init__.py
+++ __init__.py
@@ -1,19 +1,1 @@
-"""superNova_2177 package exports."""
 
-from .universe_manager import UniverseManager
-from .db_models import Base, SessionLocal
-from .ai_video_chat import VideoChatSession, create_session
-
-
-class CosmicNexus:  # pragma: no cover - placeholder for tests
-    pass
-
-
-__all__ = [
-    "UniverseManager",
-    "CosmicNexus",
-    "Base",
-    "SessionLocal",
-    "VideoChatSession",
-    "create_session",
-]
